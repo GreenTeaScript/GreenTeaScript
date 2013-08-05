cc556d3d25dec029c732dad317b1315f7db8d7a8
Masahiro Ide <imasahiro9@gmail.com>
2013-08-05 20:04:05 +0900

    fix typecheck

--- src/GreenTeaScript.java
+++ src/GreenTeaScript.java
@@ -1945,13 +1945,12 @@ final class KonohaGrammar extends GtGrammar {
 	public static TypedNode TypeBinary(TypeEnv Gamma, SyntaxTree ParsedTree, GtType Type) {
 		/*local*/TypedNode LeftNode  = ParsedTree.TypeNodeAt(LeftHandTerm, Gamma, Gamma.VarType, DefaultTypeCheckPolicy);
 		/*local*/TypedNode RightNode = ParsedTree.TypeNodeAt(RightHandTerm, Gamma, Gamma.VarType, DefaultTypeCheckPolicy);
-		/*local*/int ParamSize = 2;
 		/*local*/String Operator = ParsedTree.KeyToken.ParsedText;
 		/*local*/ArrayList<GtType> TypeList = new ArrayList<GtType>();
 		TypeList.add(LeftNode.Type);
 		TypeList.add(RightNode.Type);
-		/*local*/GtMethod Method = Gamma.NameSpace.LookupMethod(Operator, ParamSize, 1/*FIXME*/, TypeList, 0);
-		return Gamma.Generator.CreateBinaryNode(Gamma.AnyType, ParsedTree, Method, LeftNode, RightNode);
+		/*local*/GtMethod Method = Gamma.NameSpace.LookupMethod(Operator, 2, 1/*FIXME*/, TypeList, 0);
+		return Gamma.Generator.CreateBinaryNode(Method.GetReturnType(), ParsedTree, Method, LeftNode, RightNode);
 	}
 
 	public static SyntaxTree ParseField(SyntaxPattern Pattern, SyntaxTree LeftTree, TokenContext TokenContext) {
