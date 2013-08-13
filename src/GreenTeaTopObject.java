88cc66904555a4966dcee7fcecbfa82b4c5381af
kkuramitsu <kimio@konohascript.org>
2013-08-14 02:44:27 +0900

    adding GreenTeaTopObject.java

---
+++ src/GreenTeaTopObject.java
@@ -0,0 +1,24 @@
+
+public class GreenTeaTopObject {
+	/*field*/public final GtType GreenType;
+	GreenTeaTopObject/*constructor*/(GtType GreenType) {
+		this.GreenType = GreenType;
+	}
+}
+
+class GreenTeaArray extends GreenTeaTopObject {
+	GreenTeaArray/*constructor*/(GtType GreenType) {
+		super(GreenType);
+	}
+}
+
+class GreenTeaEnum extends GreenTeaTopObject {
+	/*field*/public final int EnumValue;
+	/*field*/public final String EnumSymbol;
+	GreenTeaEnum/*constructor*/(GtType GreenType, int EnumValue, String EnumSymbol) {
+		super(GreenType);
+		this.EnumValue = EnumValue;
+		this.EnumSymbol = EnumSymbol;
+	}
+}
+
