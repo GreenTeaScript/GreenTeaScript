e361a34f0881ae448fafa5d6108ba9d4b8e98ce0
MATSUMURA Tetsuro <matsumura.t.lab@gmail.com>
2013-08-14 13:16:19 +0900

    add GreenTeaTopObject to TS ver

---
+++ src/TypeScript/GreenTeaTopObject.ts
@@ -0,0 +1,25 @@
+/// <reference path="LangDeps.ts" />
+
+class GreenTeaTopObject {
+	 GreenType: GtType;
+	constructor(GreenType: GtType) {
+		this.GreenType = GreenType;
+	}
+}
+
+class GreenTeaArray extends GreenTeaTopObject {
+	constructor(GreenType: GtType) {
+		super(GreenType);
+	}
+}
+
+class GreenTeaEnum extends GreenTeaTopObject {
+	 EnumValue: number;
+	 EnumSymbol: string;
+	constructor(GreenType: GtType, EnumValue: number, EnumSymbol: string) {
+		super(GreenType);
+		this.EnumValue = EnumValue;
+		this.EnumSymbol = EnumSymbol;
+	}
+}
+
