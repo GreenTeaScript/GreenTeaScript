c7458c27caddb017799227fb159cda13c5c9fb08
Masahiro Ide <imasahiro9@gmail.com>
2013-08-14 23:04:21 +0900

    add try-catch test

---
+++ test/0030-TryCatch.green.js
@@ -0,0 +1,11 @@
+var f__05 = (function(a0) {
+   ;
+})
+(error) (eval:7) undefined name: e
+(error) (eval:7) undefined name: )
+(error) (eval:7) undefined name: {
+(error) (eval:10) undefined name: }
+(error) (eval:10) undefined name: finally
+(error) (eval:10) undefined name: {
+(error) (eval:13) undefined name: }
+(error) (eval:15) undefined name: }
