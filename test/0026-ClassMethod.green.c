5d25d6eb169ab7f06ff2e8bfa983ebc43c846839
kkuramitsu <kimio@konohascript.org>
2013-08-15 21:57:26 +0900

    moving old konoha3 test to green

--- test/0026-ClassMethod.green.c
+++ test/0026-ClassMethod.green.c
@@ -1,15 +1,15 @@
-static int f__012(X this0){
-}
 struct X{
    record __base;
 };
+static int f__012(X this0){
+}
 X;
 
-static int f__013(Y this0){
-   return 10;
-}
 struct Y{
    record __base;
 };
+static int f__013(Y this0){
+   return 10;
+}
 Y;
 
--- test/0027-ClassNew.green.c
+++ test/0027-ClassNew.green.c
@@ -1,10 +1,10 @@
+struct X{
+   record __base;
+};
 static X constructor__01205(X this0, int x1){
    ;
    return this0;
 }
-struct X{
-   record __base;
-};
 X;
 
 static void f__03(){
