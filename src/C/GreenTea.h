d25b65c68c082a177ae03dfe8dc2cbbc709f5df7
Masahiro Ide <imasahiro9@gmail.com>
2013-08-19 13:51:07 +0900

    update GreenTea.h

--- src/C/GreenTea.h
+++ src/C/GreenTea.h
@@ -1,7 +1,19 @@
 #include <assert.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include "gc.h"
 
 #ifndef GREEN_TEA_H
 #define GREEN_TEA_H
+
+typedef struct GreenTeaObject {
+  uint32_t header;
+} *GreenTeaObject;
+
+typedef struct record {
+  struct GreenTeaObject base;
+} record;
+
+#define GC_new(TYPE) ((TYPE) GC_malloc(sizeof(*(Point)NULL)))
 
 #endif /* end of include guard */
