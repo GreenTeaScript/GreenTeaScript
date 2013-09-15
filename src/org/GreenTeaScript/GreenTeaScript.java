1bf97c34e0b1aa6a2e90499c8cf25fc52f9f493a
Masahiro Ide <imasahiro9@gmail.com>
2013-09-15 09:40:30 +0900

    fix constructor call (not finished)

--- src/org/GreenTeaScript/GreenTeaScript.java
+++ src/org/GreenTeaScript/GreenTeaScript.java
@@ -3051,7 +3051,7 @@ final class GreenTeaGrammar extends GtGrammar {
 				}
 				//System.err.println("tree size = " + ParsedTree.SubTreeList.size());
 				/*local*/GtPolyFunc PolyFunc = ParsedTree.NameSpace.GetConstructorFunc(/*GtFunc*/ClassType);
-				//NodeList.set(0, Gamma.Generator.CreateNullNode(ClassType, ParsedTree));
+				NodeList.set(0, Gamma.Generator.CreateNewNode(ClassType, ParsedTree));
 				ResolvedFunc = PolyFunc.ResolveConstructor(Gamma, ParsedTree, 1, NodeList);
 				if(ResolvedFunc == null) {
 					if(!ClassType.IsNative() && ParsedTree.SubTreeList.size() == 1) {
@@ -3066,7 +3066,7 @@ final class GreenTeaGrammar extends GtGrammar {
 					NodeList.add(1, Gamma.Generator.CreateNewNode(ClassType, ParsedTree)); //JAVAONLY?
 				}
 				// TODO;
-//				return Gamma.Generator.CreateConstructorNode(ClassType, ParsedTree, ResolvedFunc, NodeList);
+				return Gamma.Generator.CreateConstructorNode(ClassType, ParsedTree, ResolvedFunc, NodeList);
 			}
 			else if(Func instanceof GtFunc) {
 				ResolvedFunc = (/*cast*/GtFunc)Func;
