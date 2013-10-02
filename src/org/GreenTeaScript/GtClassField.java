5d68e44a866755c9d7ac97a68f2cbad44660b11b
kkuramitsu <kimio@konohascript.org>
2013-10-02 21:59:38 +0900

    exporting KonohaGrammar.java to public

---
+++ src/org/GreenTeaScript/GtClassField.java
@@ -0,0 +1,99 @@
+// ***************************************************************************
+// Copyright (c) 2013, JST/CREST DEOS project authors. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// *  Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// *  Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// **************************************************************************
+
+//ifdef JAVA
+package org.GreenTeaScript;
+import java.util.ArrayList;
+//endif VAJA
+
+public final class GtClassField extends GreenTeaUtils {
+	/*field*/ public GtType DefinedType;
+	/*field*/ private GtNameSpace NameSpace;
+	/*field*/ public ArrayList<GtFieldInfo> FieldList;
+	/*field*/ public int ThisClassIndex;
+
+	GtClassField/*constructor*/(GtType DefinedType, GtNameSpace NameSpace) {
+		this.DefinedType = DefinedType;
+		this.NameSpace = NameSpace;
+		this.FieldList = new ArrayList<GtFieldInfo>();
+		/*local*/GtType SuperClass = DefinedType.SuperType;
+		if(SuperClass.TypeBody instanceof GtClassField) {
+			/*local*/GtClassField SuperField = (/*cast*/GtClassField)SuperClass.TypeBody;
+			/*local*/int i = 0;
+			while(i < SuperField.FieldList.size()) {
+				this.FieldList.add(SuperField.FieldList.get(i));
+				i+=1;
+			}
+		}
+		this.ThisClassIndex = this.FieldList.size();
+	}
+
+	public GtFieldInfo CreateField(int FieldFlag, GtType Type, String Name, GtToken SourceToken, Object InitValue) {
+		/*local*/int i = 0;
+		while(i < this.FieldList.size()) {
+			/*local*/GtFieldInfo FieldInfo = this.FieldList.get(i);
+			if(FieldInfo.Name.equals(Name)) {
+				Type.Context.ReportError(WarningLevel, SourceToken, "duplicated field: " + Name);
+				return null;
+			}
+			i = i + 1;
+		}
+		/*local*/GtFieldInfo FieldInfo = new GtFieldInfo(FieldFlag, Type, Name, this.FieldList.size(), InitValue);
+		/*local*/ArrayList<GtType> ParamList = new ArrayList<GtType>();
+		ParamList.add(FieldInfo.Type);
+		ParamList.add(this.DefinedType);
+		FieldInfo.GetterFunc = new GtFunc(GetterFunc, FieldInfo.Name, 0, ParamList);
+		this.NameSpace.SetGetterFunc(this.DefinedType, FieldInfo.Name, FieldInfo.GetterFunc, SourceToken);
+		ParamList.clear();
+		ParamList.add(Type.Context.VoidType);
+		ParamList.add(this.DefinedType);
+		ParamList.add(FieldInfo.Type);
+		FieldInfo.SetterFunc = new GtFunc(SetterFunc, FieldInfo.Name, 0, ParamList);
+		this.NameSpace.SetSetterFunc(this.DefinedType, FieldInfo.Name, FieldInfo.SetterFunc, SourceToken);
+		this.FieldList.add(FieldInfo);
+		return FieldInfo;
+	}
+}
+
+class GtFieldInfo extends GreenTeaUtils {
+	/*field*/public int     FieldFlag;
+	/*field*/public int     FieldIndex;
+	/*field*/public GtType	Type;
+	/*field*/public String	Name;
+	/*field*/public String	NativeName;
+	/*field*/public Object  InitValue;
+	/*field*/public GtFunc	GetterFunc;
+	/*field*/public GtFunc	SetterFunc;
+
+	GtFieldInfo/*constructor*/(int FieldFlag, GtType Type, String Name, int FieldIndex, Object InitValue) {
+		this.FieldFlag = FieldFlag;
+		this.Type = Type;
+		this.Name = Name;
+		this.NativeName = Name; // use this in a generator
+		this.FieldIndex = FieldIndex;
+		this.InitValue = InitValue;
+		this.GetterFunc = null;
+		this.SetterFunc = null;
+	}
+}
--- src/org/GreenTeaScript/GtTypeEnv.java
+++ src/org/GreenTeaScript/GtTypeEnv.java
@@ -200,3 +200,45 @@ public final class GtTypeEnv extends GreenTeaUtils {
 		return this.ReportTypeResult(ParsedTree, Node, TypeErrorLevel, "type error: requested = " + Type + ", given = " + Node.Type);
 	}
 }
+
+
+class GtVariableInfo extends GreenTeaUtils {
+	/*field*/public int     VariableFlag;
+	/*field*/public GtType	Type;
+	/*field*/public String	Name;
+	/*field*/public String	NativeName;
+	/*field*/public GtToken NameToken;
+	/*field*/public Object  InitValue;
+	/*field*/public int     DefCount;
+	/*field*/public int     UsedCount;
+
+	GtVariableInfo/*constructor*/(int VarFlag, GtType Type, String Name, int Index, GtToken NameToken, Object InitValue) {
+		this.VariableFlag = VarFlag;
+		this.Type = Type;
+		this.NameToken = NameToken;
+		this.Name = Name;
+		this.NativeName = (NameToken == null) ? Name : GreenTeaUtils.NativeVariableName(Name, Index);
+		this.InitValue = null;
+		this.UsedCount = 0;
+		this.DefCount  = 1;
+	}
+
+	public final void Defined() {
+		this.DefCount += 1;
+		this.InitValue = null;
+	}
+
+	public final void Used() {
+		this.UsedCount += 1;
+	}
+
+	public void Check() {
+		if(this.UsedCount == 0 && this.NameToken != null) {
+			this.Type.Context.ReportError(WarningLevel, this.NameToken, "unused variable: " + this.Name);
+		}
+	}
+	// for debug
+	@Override public String toString() {
+		return "(" + this.Type + " " + this.Name + ", " + this.NativeName + ")";
+	}
+}
