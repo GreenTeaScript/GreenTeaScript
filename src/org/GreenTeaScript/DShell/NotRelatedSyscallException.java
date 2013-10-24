7b9405c6d2b711729d1b76ad54ae746d5a4cf7c9
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2013-10-24 18:32:59 +0900

    fix dshell exception

--- src/org/GreenTeaScript/DShell/NotRelatedSyscallException.java
+++ src/org/GreenTeaScript/DShell/NotRelatedSyscallException.java
@@ -1,10 +1,10 @@
 package org.GreenTeaScript.DShell;
 
 
-public class NoRelatedSyscallException extends DShellException {
+public class NotRelatedSyscallException extends DShellException {
 	private static final long serialVersionUID = 1L;
 
-	public NoRelatedSyscallException(String message) {
+	public NotRelatedSyscallException(String message) {
 		super(message);
 	}
 	// this exception is raised by GtSubProc.runCommand 
