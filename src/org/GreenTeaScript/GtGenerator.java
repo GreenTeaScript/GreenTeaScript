58b16c87336d26756ce0573f6bba8a5fd9e2afc3
sekiguchi-nagisa <s.nagisa.xyz@gmail.com>
2013-11-08 17:13:41 +0900

    fix exception

--- src/org/GreenTeaScript/GtGenerator.java
+++ src/org/GreenTeaScript/GtGenerator.java
@@ -597,20 +597,14 @@ public class GtGenerator extends GreenTeaUtils {
 				Args[i][j] = Buffer[j];
 			}
 		}
-		
-		try {
-			if(Type.IsStringType()) {
-				return DShellProcess.ExecCommandString(Args);
-			}
-			else if(Type.equals(Type.IsBooleanType())) {
-				return DShellProcess.ExecCommandBool(Args);
-			}
-			else {
-				DShellProcess.ExecCommandVoid(Args);
-			}
-		} 
-		catch(Exception e) {
-			e.printStackTrace();
+		if(Type.IsStringType()) {
+			return DShellProcess.ExecCommandString(Args);
+		}
+		else if(Type.equals(Type.IsBooleanType())) {
+			return DShellProcess.ExecCommandBool(Args);
+		}
+		else {
+			DShellProcess.ExecCommandVoid(Args);
 		}
 //endif VAJA
 		return Node.ToNullValue(this.Context, EnforceConst);  // if unsupported
