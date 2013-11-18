9ae7cc9d3dece76b7eb3ad1d6686b7109cbd66af
takeo.onoda <takeo.onoda@gmail.com>
2013-11-18 15:30:31 +0900

    fix typescript generation

--- src/TypeScript/LibGreenTea.ts
+++ src/TypeScript/LibGreenTea.ts
@@ -15,55 +15,79 @@ interface Array {
 	iterator(): Iterator<Array>;
 }
 
-Array.prototype["size"] = function(){
-	return this.length;
-}
-
-Array.prototype["add"] = function(arg1){
-	if(arguments.length == 1) {
-		this.push(arg1);
-	} else {
-		var arg2 = arguments[1];
-		this.splice(arg1, 0, arg2);
+Object.defineProperty(Array.prototype, "size", {
+	enumerable : false,
+	value : function() {
+		return this.length;
 	}
-}
+});
 
-Array.prototype["get"] = function(i){
-	if(i >= this.length){
-		throw new RangeError("invalid array index");
+Object.defineProperty(Array.prototype, "add", {
+	enumerable : false,
+	value : function(arg1) {
+		if(arguments.length == 1) {
+			this.push(arg1);
+		} else {
+			var arg2 = arguments[1];
+			this.splice(arg1, 0, arg2);
+		}
 	}
-	return this[i];
-}
+});
 
-Array.prototype["set"] = function(i, v): void{
-	this[i] = v;
-}
-
-Array.prototype["remove"] = function(i){
-	if(i >= this.length){
-		throw new RangeError("invalid array index");
+Object.defineProperty(Array.prototype, "get", {
+	enumerable : false,
+	value : function(i) {
+		if(i >= this.length){
+			throw new RangeError("invalid array index");
+		}
+		return this[i];
 	}
-	var v = this[i];
-	this.splice(i, 1);
-	return v;
-}
+});
 
-Array.prototype["clear"] = function(){
-	this.length = 0;
-}
+Object.defineProperty(Array.prototype, "set", {
+	enumerable : false,
+	value : function(i, v) {
+		this[i] = v;
+	}
+});
+
+Object.defineProperty(Array.prototype, "remove", {
+	enumerable : false,
+	value : function(i) {
+		if(i >= this.length){
+			throw new RangeError("invalid array index");
+		}
+		var v = this[i];
+		this.splice(i, 1);
+		return v;
+	}
+});
+
+Object.defineProperty(Array.prototype, "clear", {
+	enumerable : false,
+	value : function() {
+		this.length = 0;
+	}
+});
 
 interface Object {
 	equals(other: any): boolean;
 	InstanceOf(klass: any): boolean;
 }
 
-Object.prototype["equals"] = function(other): boolean{
-	return (this === other);
-}
+Object.defineProperty(Object.prototype, "equals", {
+	enumerable : false,
+	value : function(other) {
+		return (this === other);
+	}
+});
 
-Object.prototype["InstanceOf"] = function(klass): boolean{
-	return (<any>this).constructor == klass;
-}
+Object.defineProperty(Object.prototype, "InstanceOf", {
+	enumerable : false,
+	value : function(klass) {
+		return (<any>this).constructor == klass;
+	}
+});
 
 interface String {
 	startsWith(key: string): boolean;
@@ -73,17 +97,26 @@ interface String {
 	substring(BeginIdx : number, EndIdx : number) : string;
 }
 
-String.prototype["startsWith"] = function(key): boolean{
-	return this.indexOf(key, 0) == 0;
-}
+Object.defineProperty(String.prototype, "startsWith", {
+	enumerable : false,
+	value : function(key) {
+		return this.indexOf(key, 0) == 0;
+	}
+});
 
-String.prototype["endsWith"] = function(key): boolean{
-	return this.lastIndexOf(key, 0) == 0;
-}
+Object.defineProperty(String.prototype, "endsWith", {
+	enumerable : false,
+	value : function(key) {
+		return this.lastIndexOf(key, 0) == 0;
+	}
+});
 
-String.prototype["equals"] = function(other): boolean{
-	return (this == other);
-}
+Object.defineProperty(String.prototype, "equals", {
+	enumerable : false,
+	value : function(other) {
+		return (this == other);
+	}
+});
 
 JavaScriptGlobal["GreenTeaObject"] = (function () {
     function GreenTeaObject() {
