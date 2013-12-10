f11ed70453b98da4f0a53fd425ce31714e116096
Masahiro Ide <imasahiro9@gmail.com>
2013-12-10 10:59:35 +0900

    added stub code for KonohaByteCodeGenerator

---
+++ src/org/GreenTeaScript/KonohaByteCodeGenerator.java
@@ -0,0 +1,244 @@
+package org.GreenTeaScript;
+
+public class KonohaByteCodeGenerator extends GtSourceGenerator {
+
+	public KonohaByteCodeGenerator(String TargetCode, String OutputFile, int GeneratorFlag) {
+		super(TargetCode, OutputFile, GeneratorFlag);
+	}
+
+	@Override public void VisitEmptyNode(GtEmptyNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitNullNode(GtNullNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitBooleanNode(GtBooleanNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitIntNode(GtIntNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitFloatNode(GtFloatNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitStringNode(GtStringNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitRegexNode(GtRegexNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitConstPoolNode(GtConstPoolNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitArrayLiteralNode(GtArrayLiteralNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitMapLiteralNode(GtMapLiteralNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitParamNode(GtParamNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitFunctionLiteralNode(GtFunctionLiteralNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitGetLocalNode(GtGetLocalNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitSetLocalNode(GtSetLocalNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitGetCapturedNode(GtGetCapturedNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitSetCapturedNode(GtSetCapturedNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitGetterNode(GtGetterNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitSetterNode(GtSetterNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitApplySymbolNode(GtApplySymbolNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitApplyFunctionObjectNode(GtApplyFunctionObjectNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitApplyOverridedMethodNode(GtApplyOverridedMethodNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitGetIndexNode(GtGetIndexNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitSetIndexNode(GtSetIndexNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitSliceNode(GtSliceNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitAndNode(GtAndNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitOrNode(GtOrNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitUnaryNode(GtUnaryNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitPrefixInclNode(GtPrefixInclNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitPrefixDeclNode(GtPrefixDeclNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitSuffixInclNode(GtSuffixInclNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitSuffixDeclNode(GtSuffixDeclNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitBinaryNode(GtBinaryNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitTrinaryNode(GtTrinaryNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitConstructorNode(GtConstructorNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitAllocateNode(GtAllocateNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitNewArrayNode(GtNewArrayNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitInstanceOfNode(GtInstanceOfNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitCastNode(GtCastNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitVarDeclNode(GtVarDeclNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitUsingNode(GtUsingNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitIfNode(GtIfNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitWhileNode(GtWhileNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitDoWhileNode(GtDoWhileNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitForNode(GtForNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitForEachNode(GtForEachNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitContinueNode(GtContinueNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitBreakNode(GtBreakNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitStatementNode(GtStatementNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitReturnNode(GtReturnNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitYieldNode(GtYieldNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitThrowNode(GtThrowNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitTryNode(GtTryNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitCatchNode(GtCatchNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitSwitchNode(GtSwitchNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitCaseNode(GtCaseNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitCommandNode(GtCommandNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void VisitErrorNode(GtErrorNode Node) {
+		/*FIXME*/
+	}
+
+	@Override public void OpenClassField(GtSyntaxTree ParsedTree, GtType Type, GtClassField ClassField) {
+		/*FIXME*/
+	}
+	
+	@Override public void InvokeMainFunc(String MainFuncName) {
+		/*FIXME*/
+	}
+}
